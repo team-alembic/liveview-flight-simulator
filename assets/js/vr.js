5109648932734f779e366f47ca2d2ba8144f3780
Josh Price <josh@alembic.com.au>
2022-09-27 16:10:47 +1000

    Horizon and compass interacting
    Reduced size and changed placement to reduce clipping

--- assets/js/vr.js
+++ assets/js/vr.js
@@ -12,14 +12,18 @@ export function mountVR(state, location) {
     rigElement: document.getElementById("vr-rig"),
     cameraElement: document.getElementById("vr-camera"),
     compassElement: document.getElementById("compass"),
+    horizonElement: document.getElementById("horizon"),
     pos: pos,
   }
+
+  updateVR(state, location)
 }
 
 export function updateVR(state, location) {
   let cam = state.camera.cameraElement
   let rig = state.camera.rigElement
   let compass = state.camera.compassElement
+  let horizon = state.camera.horizonElement
   let pos = {
     lat: parseFloat(location.lat),
     lng: parseFloat(location.lng),
@@ -32,6 +36,8 @@ export function updateVR(state, location) {
   rig.setAttribute("position", { x: pos.lat, y: pos.alt, z: -pos.lng })
   rig.setAttribute("rotation", { x: pos.pitch, y: -pos.yaw, z: -pos.roll })
   compass.setAttribute("rotation", { x: -90, y: pos.yaw, z: 0 })
+  horizon.setAttribute("rotation", { x: 0, y: 0, z: pos.roll })
+  horizon.setAttribute("position", { x: 0, y: -(pos.pitch / 100), z: -0.2 })
 
   // console.log(pos)
 }
